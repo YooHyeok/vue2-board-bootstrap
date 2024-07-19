0583b7cbc856cd8b839fb83ad33ef7d1488abe5d
webdevyoo <yjk7454@naver.com>
2024-07-20 00:25:39 +0900

    feat: `Board.vue` - 게시글 목록 컴포넌트 b-table 적용 (역순정렬, pk기준 이름 필터매핑)

---
+++ src/components/Board.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+  import data from '@/data';
+
+  /* 게시글번호 기준 역순(오름차순) 정렬 */
+  let items = data.Content.sort((a,b) => {return b.content_id-a.content_id})
+  items = items.map(content => {
+      console.log(content)
+      return {  // 순회한 요소 전개 후 user_name 필드 추가 (필터링된 user_name)
+        ...content,
+        user_name: data.User.filter(user => user.user_id === content.user_id)[0].name
+        // 현재 순회요소 content의 user_id와 일치하는 user객체의 user_id 추출 및 배열로 반환
+        // 배열 반환이지만 pk 기준 비교라 1개의 데이터만 반환되므로 [0]으로 접근
+      }
+    }
+  )
+  export default {
+    name: 'Board',
+    data() {
+      return {
+
+        // fields: ['content_id', `title`, `created_at`], // items로부터 보여줄 컬럼
+
+        fields: [// items로부터 보여줄 컬럼 key: 실제 컬럼 / label: 보여줄 내용
+          {
+            key: 'content_id',
+            label: '글번호',
+
+          },
+          {
+            key: 'title',
+            label: '제목'
+          }, 
+          {
+            key: `created_at`,
+            label: '작성일'
+          },
+          {
+            key: 'user_name',
+            label: '글쓴이',
+
+          },
+        ], 
+        // items: data.Content
+        items: items
+      }
+    }
+  }
+</script>
