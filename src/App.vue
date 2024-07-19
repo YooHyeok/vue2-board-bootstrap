df2980f1384874c8a9dcf6c52e4413f5898f5b4f
webdevyoo <yjk7454@naver.com>
2024-07-19 22:36:42 +0900

    feat: `Header.vue` bootstrap 헤더 컴포넌트 추가 및 App.vue에 적용

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+  }
 }
 </script>
 
@@ -18,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Vue.js로 게시판 만들기</b-navbar-brand>
+
+      <b-navbar-toggle target="nav_collapse"/> <!-- toggle 화면이 작아졌을 때 메뉴 대신 보여주는 버튼 -->
+
+      <b-collapse is-nav id="nav_collapse"> <!-- collapse - b-navbar의 toggleable 기준으로 화면이 기준보다 작아졌을 때 숨기는 기능 -->
+        <b-navbar-nav>
+          <b-nav-item href="#">공지사항</b-nav-item>
+          <b-nav-item href="#">자유게시판</b-nav-item>
+          <b-nav-item href="#">구인구직</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+  nav.navbar {
+    /* padding: 10px; */
+  }
+</style>
