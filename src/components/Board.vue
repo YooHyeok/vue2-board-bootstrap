e41ab4d77bfb12bb61fb2e040c98c1fba91ab704
webdevyoo <yjk7454@naver.com>
2024-07-22 07:38:55 +0900

    feat: `Board.vue` - 게시글 목록 컴포넌트 글쓰기 버튼 추가 및 이동 기능

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
     <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"/>
+    <b-button @click="writeContent">글쓰기</b-button>
   </div>
 </template>
 
 <script>
   import data from '@/data';
 
-  /* 게시글번호 기준 역순(오름차순) 정렬 */
-  let items = data.Content.sort((a,b) => {return b.content_id-a.content_id})
-  items = items.map(content => {
-      return {  // 순회한 요소 전개 후 user_name 필드 추가 (필터링된 user_name)
-        ...content,
-        user_name: data.User.filter(user => user.user_id === content.user_id)[0].name
-        // 현재 순회요소 content의 user_id와 일치하는 user객체의 user_id 추출 및 배열로 반환
-        // 배열 반환이지만 pk 기준 비교라 1개의 데이터만 반환되므로 [0]으로 접근
-      }
-    }
-  )
+ 
   export default {
     name: 'Board',
     data() {
+
+       /* 게시글번호 기준 역순(오름차순) 정렬 */
+      let items = data.Content.sort((a,b) => {return b.content_id-a.content_id})
+      items = items.map(content => {
+          return {  // 순회한 요소 전개 후 user_name 필드 추가 (필터링된 user_name)
+            ...content,
+            user_name: data.User.filter(user => user.user_id === content.user_id)[0].name
+            // 현재 순회요소 content의 user_id와 일치하는 user객체의 user_id 추출 및 배열로 반환
+            // 배열 반환이지만 pk 기준 비교라 1개의 데이터만 반환되므로 [0]으로 접근
+          }
+        }
+      )
+
       return {
 
         // fields: ['content_id', `title`, `created_at`], // items로부터 보여줄 컬럼
@@ -54,7 +58,13 @@
         this.$router.push({
           path: `/board/free/detail/${item.content_id}`
         })
+      },
+      writeContent() {
+      this.$router.push({
+          path: '/board/free/create'
+        })
       }
-    }
+    },
+    
   }
 </script>
