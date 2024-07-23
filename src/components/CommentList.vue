fe104ca44bf4d361d5cb776e9ba754857200011e
webdevyoo <yjk7454@naver.com>
2024-07-24 00:10:42 +0900

    feat: `CommentList.vue` - 댓글 아이템 목록 컴포넌트 추가 및 구현

---
+++ src/components/CommentList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div :key="item.comment_id" v-for="item in comments">
+      <div>
+        <CommentListItem :commentObj="item"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import data from '@/data'
+import CommentListItem from '@/components/CommentListItem.vue';
+export default {
+  name: 'CommentList',
+  components: {
+    CommentListItem
+  },
+  props: {
+    contentId: Number,
+  },
+  data() {
+    return {
+      comments: data.Comment.filter(item => item.content_id === this.contentId)
+    }
+  }
+}
+</script>
