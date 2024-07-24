6c630be6aa778ed575ea2f8f226310d53d393ea0
webdevyoo <yjk7454@naver.com>
2024-07-25 00:02:29 +0900

    feat: `CommentList.vue` - 댓글 작성 컴포넌트 적용 및 댓글 리로드 기능 구현

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <div :key="item.comment_id" v-for="item in comments">
-      <div>
         <CommentListItem :commentObj="item"/>
-      </div>
     </div>
+    <CommentCreate 
+    :contentId="contentId"
+    :reloadComment="reloadComment"
+    />
   </div>
 </template>
 <script>
 import data from '@/data'
 import CommentListItem from '@/components/CommentListItem.vue';
+import CommentCreate from '@/components/CommentCreate.vue';
+
 export default {
   name: 'CommentList',
   components: {
-    CommentListItem
+    CommentListItem,
+    CommentCreate
   },
   props: {
     contentId: Number,
@@ -21,6 +26,15 @@ export default {
   data() {
     return {
       comments: data.Comment.filter(item => item.content_id === this.contentId)
+    }
+  },
+  methods: {
+    /**
+     * 댓글 리스트를 리로드 한다.
+     * comments 최신 data로부터 다시 초기화 한다.
+     */
+    reloadComment() {
+      this.comments = data.Comment.filter(item => item.content_id === this.contentId)
     }
   }
 }
