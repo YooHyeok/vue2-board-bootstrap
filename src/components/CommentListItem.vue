35365e214f387b6ee00da24bc8814931eaf526ae
webdevyoo <yjk7454@naver.com>
2024-07-25 20:17:05 +0900

    feat: `CommentListItem.vue` - 대 댓글 목록 데이터 구성 및 컴포넌트 출력

--- src/components/CommentListItem.vue
+++ src/components/CommentListItem.vue
@@ -11,6 +11,24 @@
         <b-button variant="info">삭제</b-button>
       </div>
     </div>
+    <!-- 대댓글 -->
+    <template v-if="subCommentList.length > 0">
+      <div
+        class="comment-list-item-subcomment-list"
+        :key="subComment.subcomment_id"
+        v-for="subComment in subCommentList"
+      >
+      <div class="comment-list-item-name">
+        <div>{{subComment.user_name}}</div>
+        <div>{{subComment.created_at}}</div>
+      </div>
+      <div class="comment-list-item-context">{{subComment.context}}</div>
+      <div class="comment-list-item-button">
+        <b-button variant="info">수정</b-button>
+        <b-button variant="info">삭제</b-button>
+      </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -25,6 +43,15 @@ export default {
   data() {
     return {
       userName: data.User.filter(item => item.user_id === this.commentObj.user_id)[0].name,
+      subCommentList: data.SubComment
+      .filter(item => item.comment_id === this.commentObj.comment_id) // 대댓글 필터링 - 부모 댓글의 commentId PK와 일치하는 commentId 기준
+      .map(subCommentItem => ( // user_id가 일치하는 name 저장 (객체 반환을 위해 괄호로 묶음.)
+          {
+            ...subCommentItem,
+            user_name: data.User.filter(item => item.user_id === subCommentItem.user_id)[0].name
+          }
+        )
+      )
     };
   },
 
